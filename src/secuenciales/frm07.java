15e52bec6e13000bb0fe245e7e24bc9c7d2449fe
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-11-01 20:34:19 -0500

    07

--- src/secuenciales/frm07.java
+++ src/secuenciales/frm07.java
@@ -54,7 +54,45 @@ public class frm07 extends JFrame {
         txtBase.setMargin(new Insets(5, 5, 5, 5));
         getContentPane().add(txtBase);
 
+        txtAltura = new JTextField();
+        txtAltura.setBounds(150, 90, 80, 30);
+        txtAltura.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtAltura.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtAltura);
 
+        txtArea = new JTextField();
+        txtArea.setBounds(150, 130, 80, 30);
+        txtArea.setFocusable(false);
+        txtArea.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtArea.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtArea);
+
+        txtPerimetro = new JTextField();
+        txtPerimetro.setBounds(150, 170, 80, 30);
+        txtPerimetro.setFocusable(false);
+        txtPerimetro.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtPerimetro.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtPerimetro);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(100, 270, 100, 30);
+        btnCalcular.setMnemonic('a');
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
+    }
+
+    protected void btnCalcular_actionPerformed() {
+        
+            double base = Double.parseDouble(txtBase.getText());
+            double altura = Double.parseDouble(txtAltura.getText());
+
+            double area = base * altura;
+            double perimetro = 2 * (base + altura);
+
+            DecimalFormat df = new DecimalFormat("###.##");
+            txtArea.setText(df.format(area));
+            txtPerimetro.setText(df.format(perimetro));
 
     }
 }
