6b89ae0d15c2d59a087909c9f962de58e78e6452
CristianQSS8113 <cristianeduardo1812@gmail.com>
2024-11-03 00:51:38 -0500

    sd

--- src/condicionales/frm39.java
+++ src/condicionales/frm39.java
@@ -1,5 +1,109 @@
 package condicionales;
 
-public class frm39 {
-    
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class frm39 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtHorasAusencia, txtTornillosDefectuosos, txtTornillosNoDefectuosos, txtGradoEficiencia;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                frm39 frame = new frm39();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public frm39() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 400, 300);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblHorasAusencia = new JLabel("Horas de Ausencia:");
+        lblHorasAusencia.setBounds(50, 50, 150, 30);
+        getContentPane().add(lblHorasAusencia);
+
+        JLabel lblTornillosDefectuosos = new JLabel("Tornillos Defectuosos:");
+        lblTornillosDefectuosos.setBounds(50, 90, 150, 30);
+        getContentPane().add(lblTornillosDefectuosos);
+
+        JLabel lblTornillosNoDefectuosos = new JLabel("Tornillos No Defectuosos:");
+        lblTornillosNoDefectuosos.setBounds(50, 130, 150, 30);
+        getContentPane().add(lblTornillosNoDefectuosos);
+
+        JLabel lblGradoEficiencia = new JLabel("Grado de Eficiencia:");
+        lblGradoEficiencia.setBounds(50, 170, 150, 30);
+        getContentPane().add(lblGradoEficiencia);
+
+        txtHorasAusencia = new JTextField();
+        txtHorasAusencia.setBounds(220, 50, 100, 30);
+        txtHorasAusencia.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtHorasAusencia);
+
+        txtTornillosDefectuosos = new JTextField();
+        txtTornillosDefectuosos.setBounds(220, 90, 100, 30);
+        txtTornillosDefectuosos.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtTornillosDefectuosos);
+
+        txtTornillosNoDefectuosos = new JTextField();
+        txtTornillosNoDefectuosos.setBounds(220, 130, 100, 30);
+        txtTornillosNoDefectuosos.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtTornillosNoDefectuosos);
+
+        txtGradoEficiencia = new JTextField();
+        txtGradoEficiencia.setBounds(220, 170, 100, 30);
+        txtGradoEficiencia.setFocusable(false);
+        txtGradoEficiencia.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtGradoEficiencia);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(150, 210, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> calcularGradoEficiencia());
+    }
+
+    protected void calcularGradoEficiencia() {
+        try {
+            double horasAusencia = Double.parseDouble(txtHorasAusencia.getText());
+            int tornillosDefectuosos = Integer.parseInt(txtTornillosDefectuosos.getText());
+            int tornillosNoDefectuosos = Integer.parseInt(txtTornillosNoDefectuosos.getText());
+
+            boolean cumpleCond1 = horasAusencia <= 1.5;
+            boolean cumpleCond2 = tornillosDefectuosos < 300;
+            boolean cumpleCond3 = tornillosNoDefectuosos > 10000;
+            int gradoEficiencia;
+
+            if (cumpleCond1 && cumpleCond2 && cumpleCond3) {
+                gradoEficiencia = 20;
+            } else if (cumpleCond1 && cumpleCond2) {
+                gradoEficiencia = 12;
+            } else if (cumpleCond1 && cumpleCond3) {
+                gradoEficiencia = 13;
+            } else if (cumpleCond2 && cumpleCond3) {
+                gradoEficiencia = 15;
+            } else if (cumpleCond1) {
+                gradoEficiencia = 7;
+            } else if (cumpleCond2) {
+                gradoEficiencia = 8;
+            } else if (cumpleCond3) {
+                gradoEficiencia = 9;
+            } else {
+                gradoEficiencia = 5;
+            }
+
+            txtGradoEficiencia.setText(String.valueOf(gradoEficiencia));
+        } catch (Exception ex) {
+            txtGradoEficiencia.setText("Error");
+        }
+    }
 }
--- src/condicionales/frm40.java
+++ src/condicionales/frm40.java
@@ -1,5 +1,140 @@
 package condicionales;
 
-public class frm40 {
-    
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+
+public class frm40 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtPC1, txtPC2, txtPC3, txtEP, txtEF, txtPromedio, txtCondicion;
+    JComboBox<String> comboCurso;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                frm40 frame = new frm40();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public frm40() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 450, 350);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblCurso = new JLabel("Curso:");
+        lblCurso.setBounds(50, 30, 150, 30);
+        getContentPane().add(lblCurso);
+
+        comboCurso = new JComboBox<>(new String[] {"Matemática", "Física", "Química"});
+        comboCurso.setBounds(200, 30, 150, 30);
+        getContentPane().add(comboCurso);
+
+        JLabel lblPC1 = new JLabel("Nota PC1:");
+        lblPC1.setBounds(50, 70, 150, 30);
+        getContentPane().add(lblPC1);
+
+        JLabel lblPC2 = new JLabel("Nota PC2:");
+        lblPC2.setBounds(50, 110, 150, 30);
+        getContentPane().add(lblPC2);
+
+        JLabel lblPC3 = new JLabel("Nota PC3:");
+        lblPC3.setBounds(50, 150, 150, 30);
+        getContentPane().add(lblPC3);
+
+        JLabel lblEP = new JLabel("Examen Parcial:");
+        lblEP.setBounds(50, 190, 150, 30);
+        getContentPane().add(lblEP);
+
+        JLabel lblEF = new JLabel("Examen Final:");
+        lblEF.setBounds(50, 230, 150, 30);
+        getContentPane().add(lblEF);
+
+        JLabel lblPromedio = new JLabel("Promedio Final:");
+        lblPromedio.setBounds(50, 270, 150, 30);
+        getContentPane().add(lblPromedio);
+
+        txtPC1 = new JTextField();
+        txtPC1.setBounds(200, 70, 100, 30);
+        txtPC1.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtPC1);
+
+        txtPC2 = new JTextField();
+        txtPC2.setBounds(200, 110, 100, 30);
+        txtPC2.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtPC2);
+
+        txtPC3 = new JTextField();
+        txtPC3.setBounds(200, 150, 100, 30);
+        txtPC3.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtPC3);
+
+        txtEP = new JTextField();
+        txtEP.setBounds(200, 190, 100, 30);
+        txtEP.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtEP);
+
+        txtEF = new JTextField();
+        txtEF.setBounds(200, 230, 100, 30);
+        txtEF.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtEF);
+
+        txtPromedio = new JTextField();
+        txtPromedio.setBounds(200, 270, 100, 30);
+        txtPromedio.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtPromedio.setFocusable(false);
+        getContentPane().add(txtPromedio);
+
+        txtCondicion = new JTextField();
+        txtCondicion.setBounds(320, 270, 100, 30);
+        txtCondicion.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtCondicion.setFocusable(false);
+        getContentPane().add(txtCondicion);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(320, 70, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> calcularPromedio());
+    }
+
+    protected void calcularPromedio() {
+        try {
+            double pc1 = Double.parseDouble(txtPC1.getText());
+            double pc2 = Double.parseDouble(txtPC2.getText());
+            double pc3 = Double.parseDouble(txtPC3.getText());
+            double ep = Double.parseDouble(txtEP.getText());
+            double ef = Double.parseDouble(txtEF.getText());
+
+            String curso = (String) comboCurso.getSelectedItem();
+            double promedioFinal = 0.0;
+
+            switch (curso) {
+                case "Matemática":
+                    promedioFinal = pc1 * 0.10 + pc2 * 0.20 + pc3 * 0.10 + ep * 0.30 + ef * 0.30;
+                    break;
+                case "Física":
+                    promedioFinal = pc1 * 0.20 + pc2 * 0.20 + pc3 * 0.20 + ep * 0.20 + ef * 0.20;
+                    break;
+                case "Química":
+                    promedioFinal = pc1 * 0.10 + pc2 * 0.30 + pc3 * 0.10 + ep * 0.25 + ef * 0.25;
+                    break;
+            }
+
+            txtPromedio.setText(String.format("%.2f", promedioFinal));
+            txtCondicion.setText(promedioFinal >= 13 ? "Aprobado" : "Desaprobado");
+
+        } catch (Exception ex) {
+            txtPromedio.setText("Error");
+            txtCondicion.setText("Error");
+        }
+    }
 }
